5b7c7a93e4e095a3f8ffb4222b11a603d4ec33ae
Aleksei Kudinov <reanim86@yandex.ru>
2023-08-31 19:58:23 +0300

    add 2 tests

--- test/backend/test_backend_api.py
+++ test/backend/test_backend_api.py
@@ -1,16 +1,31 @@
 import pytest
+from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient
 
+from backend.models import User
+import json
 
-
-
-
-@pytest.mark.django_db
-def test_add_user_upload_data():
-    client = APIClient()
-    response = client.post('/upload/', {'url': 'http://gw36.a-don.ru:8181/platform/shop1.yaml'})
-    assert response.status_code == 200
 
 @pytest.mark.django_db
 def test_add_user():
-    pass
+    count = User.objects.count()
+    client = APIClient()
+    data = json.dumps({'surname': 'kudino',
+                        'first_name': 'Alekse',
+                        'last_name': 'Ivanovch',
+                        'email': 'a.kudino@a-don.ru',
+                        'password': 'test',
+                        'confirm_password': 'test',
+                        'company': 'IP Ivanov',
+                        'position': 'CEO'})
+    response = client.post(path='/createuser/',data=data, content_type="application/json")
+    assert User.objects.count() == count + 1
+
+    @pytest.mark.django_db
+    def test_add_user_upload_data():
+        token = Token.objects.get(id=1)
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        response = client.post('/upload/', {'url': 'http://gw36.a-don.ru:8181/platform/shop1.yaml'})
+        client.credentials()
+        assert response.status_code == 200
